37f731ae69570e0b4c060bedc5ed62c458b32359
Marco Walther <marco@sonic.net>
2019-06-02 20:37:57 -0700

    More RC receiver changes

--- SGVHAK_Rover/rc_receiver.py
+++ SGVHAK_Rover/rc_receiver.py
@@ -8,8 +8,10 @@ import threading
 import time
 import tty
 
+import configuration
+
 class RCReader(threading.Thread):
-    ser = None
+    sp = None
     start_called = False
     chassis = None
 
@@ -20,6 +22,7 @@ class RCReader(threading.Thread):
         self.args = args
         self.kwargs = kwargs
         self.chassis = args[0]
+        self.on = True
 
         return
 
@@ -28,24 +31,40 @@ class RCReader(threading.Thread):
             raise RunTimeError
 
         self.start_called = True
-        self.fin = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
 
+        # Read parameter file
+        config = configuration.configuration("rc_receiver")
+        connectparams = config.load()['connect']
+        
+        # Open serial port with parameters
+        s = serial.Serial()
+        s.baudrate = connectparams['baudrate']
+        s.port = connectparams['port']
+        s.timeout = connectparams['timeout']
+        s.open()
+        
+        if s.is_open:
+            self.sp = s
 
-        i = 0                              # Just read some lines
-        line = self.fin.readline()
-        while line and i < 10:
-            line = self.fin.readline()
-            i += 1
+            i = 0                              # Just read some lines
+            line = self.sp.readline()
+            while line and i < 10:
+                line = self.sp.readline()
+                i += 1
 
-        super(RCReader, self).start()
+            super(RCReader, self).start()
+
+    def cancel(self):
+        self.on = False
 
     def run(self):
         last_time = datetime.datetime(1970, 1, 1)
         rc_use_input_time = datetime.datetime(1970, 1, 1)
         send_stop = 0
+        last_rc = [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ]
 
-        while True:
-            line = self.fin.readline()
+        while self.on:
+            line = self.sp.readline()
 
             m = re.match('I ([-\d.]+) +([-\d.]+) +([-\d.]+) +([-\d.]+) +([-\d.]+) +([-\d.]+) +([-\d.]+) +([-\d.]+)', line)
             if m != None:
@@ -53,10 +72,37 @@ class RCReader(threading.Thread):
 
                 if (t - last_time).total_seconds() > 0.05:
                     last_time = t
+                    # logging.getLogger('werkzeug').error("%s: rc_receiver.run(%f, %f, %f)" % (datetime.datetime.utcnow().strftime("%H%M%S.%f"), float(m.group(1)), float(m.group(3)), float(m.group(5))))
+
                     angle = (float(m.group(1)) - 1500.0) / 5.0
                     throttle = (float(m.group(3)) - 1500.0) / 5.0
                     rc_use = float(m.group(5)) - 1500.0
-                    
+
+                    if rc_use > -450 and rc_use < 450:
+                        logging.getLogger('werkzeug').error("%s: rc_receiver.run() messed up last: %f, %f, %f, %f, %f, %f, %f, %f  now %f, %f, %f, %f, %f, %f, %f, %f" %
+                            (datetime.datetime.utcnow().strftime("%H%M%S.%f"),
+                             last_rc[0], last_rc[1], last_rc[2], last_rc[3], last_rc[4], last_rc[5], last_rc[6], last_rc[7],
+                             float(m.group(1)), float(m.group(2)), float(m.group(3)), float(m.group(4)), float(m.group(5)), float(m.group(6)), float(m.group(7)), float(m.group(8))))
+
+                        last_rc[0] = float(m.group(1))
+                        last_rc[1] = float(m.group(2))
+                        last_rc[2] = float(m.group(3))
+                        last_rc[3] = float(m.group(4))
+                        last_rc[4] = float(m.group(5))
+                        last_rc[5] = float(m.group(6))
+                        last_rc[6] = float(m.group(7))
+                        last_rc[7] = float(m.group(8))
+                        continue
+
+                    last_rc[0] = float(m.group(1))
+                    last_rc[1] = float(m.group(2))
+                    last_rc[2] = float(m.group(3))
+                    last_rc[3] = float(m.group(4))
+                    last_rc[4] = float(m.group(5))
+                    last_rc[5] = float(m.group(6))
+                    last_rc[6] = float(m.group(7))
+                    last_rc[7] = float(m.group(8))
+
                     if rc_use < 0.0:
                         self.chassis.use_rc_input = False
                         rc_use_input_time =  datetime.datetime(1970, 1, 1)
@@ -102,6 +148,8 @@ class RCReader(threading.Thread):
 
             if (datetime.datetime.utcnow() - rc_use_input_time).total_seconds() > 2.0:
                 self.chassis.use_rc_input = False
+
+        self.sp.close()
 
 
 # t = RCReader()
